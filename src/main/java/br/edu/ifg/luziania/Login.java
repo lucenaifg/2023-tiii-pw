c3341fc8502698234445d32770f19d2228b8fe14
Daniel Lucena <cto@mindify.net>
2023-04-10 14:26:55 -0300

    Requisições com FetchAPI.

--- src/main/java/br/edu/ifg/luziania/Login.java
+++ src/main/java/br/edu/ifg/luziania/Login.java
@@ -3,17 +3,17 @@ package br.edu.ifg.luziania;
 import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+//@Path -> avisa o servidor de aplicação que essa classe java pode receber requisições
+// e defini o caminho base para essa classe.
 @Path("")
 public class Login {
 
     //Atributo que recupera o html na pasta resources
     private final Template login;
-
 
     //Construtor de inicialização do template
     public Login(Template login) {
@@ -21,15 +21,25 @@ public class Login {
     }
 
     @GET
-    @Path("/ok")
-    public String ok(){
-        return "ok";
-    }
-
-    @GET
     @Produces(MediaType.TEXT_HTML)
-    @Path("/login")
+    @Path("")
     public TemplateInstance login(){
         return login.instance();
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/autenticar")
+    public Response autenticar(Autenticacao autenticacao){
+        RetornoAutenticacao retorno = new RetornoAutenticacao();
+        if (autenticacao.getEmail().equals("daniel@ifg.edu.br") && autenticacao.getSenha().equals("123")) {
+            retorno.setMessagem("Usuário autenticado!");
+            return Response.ok(retorno, MediaType.APPLICATION_JSON).build();
+        }
+        else {
+            retorno.setMessagem("Usuário não autenticado!");
+            return Response.ok(retorno, MediaType.APPLICATION_JSON).build();
+        }
     }
 }
---
+++ src/main/resources/META-INF/resources/js/login.js
@@ -0,0 +1,33 @@
+function validar_formulario(){
+    return document.getElementById("email").value !== '' && document.getElementById("senha").value !== '';
+}
+
+function autenticar(){
+    if (validar_formulario()){
+        var requisicao = criarRequisicao(document.getElementById("email").value,document.getElementById("senha").value);
+        fetch(requisicao)
+        .then((response) => {
+            if (response.status === 200) {
+                return response.json();
+            } else {
+                throw new Error("Ocorreu algum erro no servidor!");
+            }
+        })
+        .then(json => console.log(JSON.stringify(json)));
+    } else
+        alert('Os campos e-mail e senha são obrigatórios! Verifique o formulário.')
+}
+
+function criarRequisicao(email, senha){
+    return new Request("http://localhost:8080/autenticar", {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "email": email,
+            "senha": senha
+        }),
+    });
+}
